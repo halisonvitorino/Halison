cfd2d5b3e51c068ab96bcd01720392de9bd16a25
Halison Ribeiro Vitorino <halisonvitorino@yahoo.com.br>
2023-06-15 21:49:47 -0300

    ver2

---
+++ Projetos-front-end-java-noturno/filmes/src/main/java/controller/PageController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	
+	  @RequestMapping("/")
+	  String index() {
+	    return "index";
+	  }
+	  
+	  @RequestMapping("/cadastrar")
+	  String cadastrar() {
+	    return "cadastrar";
+	  }
+
+}
